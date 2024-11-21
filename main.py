5872e3b534b58eb472f51323dcc47790818b6398
Victor Akahane <victor.s.akahane@gmail.com>
2024-11-21 11:20:45 -0300

    FEAT: Added user password encrypt

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
+bcrypt = Bcrypt(app)
 from views.views_user import *
 from views.views_game import *
 if __name__ == '__main__':
