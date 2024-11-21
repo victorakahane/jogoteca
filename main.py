38e0565c8d43c58722a60ea267e2dac2c3e22c72
Victor Akahane <victor.s.akahane@gmail.com>
2024-11-21 10:45:22 -0300

    REFACTOR: Refactored design and routes New module Views: view_game for game routes and view_user for user routes Design: Enhancements on design for templates PEP8 from pylint enhancements

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
-from views import *
-
+from views.views_user import *
+from views.views_game import *
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
